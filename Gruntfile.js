cd11dc7fd31fe1968898b207dd89c802b12660fb
Ristomatti Airo <ristomatti@kallio.io>
2017-05-21 00:50:48 +0300

    Process custom styles in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -205,6 +205,14 @@ module.exports = function(grunt) {
                         "editor/custom/css/node-red-solarized-nodes.css"
                     ]
                 }
+            },
+            custom: {
+                files: {
+                    "public/custom/style.css": [
+                        "editor/custom/css/node-red-solarized-dark-grey.css",
+                        "editor/custom/css/node-red-solarized-nodes.css"
+                    ]
+                }
             }
         },
         uglify: {
