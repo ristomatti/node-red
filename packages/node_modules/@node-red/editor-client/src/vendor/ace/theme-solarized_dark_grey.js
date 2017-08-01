a1de840deb9a5e3ced46e9c2bc704f36f5c02d57
Ristomatti Airo <ristomatti@kallio.io>
2017-08-01 16:59:12 +0300

    Update Solarized Dark Grey Ace theme define syntax

--- packages/node_modules/@node-red/editor-client/src/vendor/ace/theme-solarized_dark_grey.js
+++ packages/node_modules/@node-red/editor-client/src/vendor/ace/theme-solarized_dark_grey.js
@@ -1,4 +1,4 @@
-define("ace/theme/solarized_dark_grey",["require","exports","module","ace/lib/dom"], function(require, exports, module) {
+ace.define("ace/theme/solarized_dark_grey",["require","exports","module","ace/lib/dom"], function(require, exports, module) {
 
 exports.isDark = true;
 exports.cssClass = "ace-solarized-dark-grey";
